e0683af6add1199bc8384c1d51203d3f3bcff048
Cole Stewart <Cole.Stewart@arm.com>
2021-11-18 12:03:22 -0600

    Fix contact form issues

--- public/contactform/contactform.php
+++ public/contactform/contactform.php
@@ -1,13 +1,12 @@
 <?php
-
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["contact-name"]));
+        $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["contact-email"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["contact-subject"]);
-        $message = trim($_POST["contact-message"]);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $subject = trim($_POST["subject"]);
+        $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -45,5 +44,4 @@
         http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
-
 ?>
